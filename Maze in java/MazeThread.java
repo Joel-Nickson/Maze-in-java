97f4b3f848a2a9e957483ebbaa972d376de315aa
Joel Nickson <joel2000nickson@gmail.com>
2021-02-17 17:06:27 +0530

    removing last update(due to problems with jpanel)

--- Maze in java/MazeThread.java
+++ Maze in java/MazeThread.java
@@ -557,7 +557,6 @@ class Maze extends JFrame implements ActionListener,MouseListener,MouseMotionLis
 
           
   public void paint(Graphics g){
-    super.paint(g);
     if(redraw<1){
       for(int i=0;i<rows;i++){
         for(int j=0;j<columns;j++){
